9f4507ab74b056a37d0f7edb401512480c7b1cfe
Yugiobattu420 <nhatduy0409@gmail.com>
2022-08-03 09:29:13 +0700

    update BE - Add task routes

--- NodeJS/routes/routesApp.js
+++ NodeJS/routes/routesApp.js
@@ -12,7 +12,7 @@ const initRoutesApp = (app) => {
     router.post('/accounts/register', accountController.register)
     router.post('/accounts/updateUser', accountController.updateUserInformation)
     router.post('/accounts/deleteUser', accountController.deleteUser)
-    
+
 
     router.post('/projects/showAllProject', projectController.showAllProject)
     router.post('/projects/showProjectInfomation', projectController.showProjectInfomation)
@@ -30,14 +30,29 @@ const initRoutesApp = (app) => {
 
 
 
-    // router.post('/tasks/getTasks', tasksController.getTasks)
-    // router.post('/tasks/addTasks', tasksController.addTasks)
-    // router.post('/tasks/deleteTasks', tasksController.deleteTasks)
-    // router.post('/tasks/updateTasks', tasksController.updateTasks)
-    // router.post('/tasks/updateTaskProjectList', tasksController.updateTaskProjectList)
-    // router.post('/tasks/updateTaskStatus', tasksController.updateTaskStatus)
-    
- 
+    router.post('/tasks/getTasks', tasksController.showUserTasks)
+    router.post('/tasks/addTasks', tasksController.addTasks)
+    router.post('/tasks/deleteTasks', tasksController.deleteTasks)
+    router.post('/tasks/updateTasks', tasksController.updateTasks)
+
+    router.post('/tasks/addSubtasks', tasksController.addSubtasks)
+    router.post('/tasks/deleteSubtasks', tasksController.deleteSubtasks)
+
+    router.post('/tasks/addNotification', tasksController.addNotification)
+    router.post('/tasks/deleteNotification', tasksController.deleteNotification)
+
+    router.post('/tasks/addTag', tasksController.addTag)
+    router.post('/tasks/deleteTag', tasksController.deleteTag)
+    router.post('/tasks/deleteAllTag', tasksController.deleteAllTag)
+
+    router.post('/tasks/addTaskParticipant', tasksController.addTaskParticipant)
+    router.post('/tasks/deleteTaskParticipant', tasksController.deleteTaskParticipant)
+
+    router.post('/tasks/getProjectTasks', tasksController.showProjectTasks)
+    router.post('/tasks/updateTaskProjectList', tasksController.updateTaskProjectList)
+    router.post('/tasks/updateTaskStatus', tasksController.updateTaskStatus)
+
+
     return app.use("/", router);
 }
 
